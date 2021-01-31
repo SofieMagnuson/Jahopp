154a4315ff1902fd1143944a7e8d19efaddff352
SofieMagnuson <60152591+SofieMagnuson@users.noreply.github.com>
2021-01-31 21:30:04 +0100

    jump sounds & start on tutorial

---
+++ NoSenoresPorFavor2D/Assets/TutorialManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TutorialManager : MonoBehaviour
+{
+
+    public GameObject[] popUps;
+    private int popUpIndex;
+
+    void Update()
+
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[popUpIndex].SetActive(true);
+
+            }
+
+            if (popUpIndex == 0)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    popUpIndex++;
+                }
+
+                else if (popUpIndex == 1)
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        popUpIndex++;
+
+                    }
+
+                    else if (popUpIndex == 2)
+                    {
+                        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
