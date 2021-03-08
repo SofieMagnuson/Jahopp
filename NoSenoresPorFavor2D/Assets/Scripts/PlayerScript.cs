a8b87c4339dea73e20aa9d7e4d9ad1fb25bebd19
SofieMagnuson <60152591+SofieMagnuson@users.noreply.github.com>
2021-03-08 18:48:58 +0100

    Changed level 2 and added lvl 4 and winning condition

--- NoSenoresPorFavor2D/Assets/Scripts/PlayerScript.cs
+++ NoSenoresPorFavor2D/Assets/Scripts/PlayerScript.cs
@@ -25,10 +25,10 @@ public class PlayerScript : MonoBehaviour
         myRB = GetComponent<Rigidbody2D>();
         mySR = GetComponent<SpriteRenderer>();
 
-        jumpTimer = 1f;
-        playerSpeed = 12f;
+        jumpTimer = 0f;
+        playerSpeed = 8f;
         rollingSpeed = 10f;
-        rollingTimer = 0.5f;
+        //rollingTimer = 0.5f;
         playerJump = 40f;
         isRolling = false;
         isGrounded = false;
@@ -56,37 +56,38 @@ public class PlayerScript : MonoBehaviour
         {
             mySR.flipX = true;
         }
-        if (Input.GetKey(KeyCode.Space) && (Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Horizontal") == 1))
-        {
-            isRolling = true;
-        }
-        else
-        {
-            isRolling = false;
+       
+        // if (Input.GetKey(KeyCode.Space) && (Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Horizontal") == 1))
+       // {
+        //    isRolling = true;
+        //}
+        //else
+        //{
+        //    isRolling = false;
           
-        }
+        //}
 
       
-        if (rollingTimer <= 0)
-        {
-            playerSpeed = rollingSpeed;
-        }
+        //if (rollingTimer <= 0)
+        //{
+        //    playerSpeed = rollingSpeed;
+        //}
 
-        if (isRolling)
-        {
-            jumpTimer -= Time.deltaTime;
-            rollingTimer -= Time.deltaTime;
-        }
-        else
-        {
-            rollingTimer = 0.5f;
-            if (isGrounded)
-            {
-                jumpTimer = 1f;
-                playerSpeed = 6f;
-            
-            }
-        }
+        //if (isRolling)
+        //{
+        //    jumpTimer -= Time.deltaTime;
+        //    rollingTimer -= Time.deltaTime;
+        //}
+        //else
+        //{
+        //    rollingTimer = 0.5f;
+        //    if (isGrounded)
+        //    {
+        //        jumpTimer = 1f;
+        //        playerSpeed = 6f;
+        //    
+        //    }
+        //}
 
         if (isShowingGlass)
         {
